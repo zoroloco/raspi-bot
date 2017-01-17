27bd61562a0d45020d4e2ad21128cbb6ede48143
Kenneth Centurion <kcenturion@yahoo.com>
2017-01-16 20:44:49 -0600

    s

--- scripts/shell/raspy-clean-build.sh
+++ scripts/shell/raspy-clean-build.sh
@@ -20,18 +20,18 @@ cd $SRC_DIR
 sudo npm install
 
 echo "making scripts executable..."
-sudo chmod +x $SRC_DIR/app/scripts/shell/raspy-run.sh
-sudo chmod +x $SRC_DIR/app/scripts/shell/raspy-update.sh
+sudo chmod +x $SRC_DIR/scripts/shell/raspy-run.sh
+sudo chmod +x $SRC_DIR/scripts/shell/raspy-update.sh
 
 echo "moving and updating startup daemon script..."
 #sudo rm -rf /usr/lib/systemd/system/raspy.service
-sudo chmod +x $SRC_DIR/app/scripts/shell/raspy.service
-sudo cp $SRC_DIR/app/scripts/shell/raspy.service /usr/lib/systemd/system/
+sudo chmod +x $SRC_DIR/scripts/shell/raspy.service
+sudo cp $SRC_DIR/scripts/shell/raspy.service /usr/lib/systemd/system/
 cd /etc/systemd/system
 sudo ln -s /usr/lib/systemd/system/raspy.service ./raspy.service
 sudo systemctl daemon-reload
 
 echo "Now changing permissions and running raspy..."
-sudo chmod +x $SRC_DIR/app/scripts/shell/raspy-run.sh
+sudo chmod +x $SRC_DIR/scripts/shell/raspy-run.sh
 sudo systemctl start raspy.service
 #sudo $SRC_DIR/scripts/shell/druidia-run.sh
